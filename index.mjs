11a203b36c2b4bf1badbe29fa54bfd946b8eb91a
saribghouri <sarib.ghouri92@gmail.com>
2022-08-04 08:03:22 -0700

    server created

--- index.mjs
+++ index.mjs
@@ -1,10 +1,24 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import cors from "cors";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app = express();
+app.use(cors());
+
+app.get("/home", (req, res) => {
+  res.send("I am home page!");
 });
+
+app.get("/profile", (req, res) => {
+  res.send("I am profile page!");
+});
+app.get("/weather", (req, res) => {
+  res.send({
+    weather: "sunny",
+    temperature: "20",
+    city: "London",
+  });
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
